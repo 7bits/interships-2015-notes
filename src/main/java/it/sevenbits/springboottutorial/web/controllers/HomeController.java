d8c0da7569224eb47a5c4103354882c5b76d3701
vik <xperia.v.j.p@gmail.com>
2015-07-13 16:45:06 +0600

    Restyle of main page

--- src/main/java/it/sevenbits/springboottutorial/web/controllers/HomeController.java
+++ src/main/java/it/sevenbits/springboottutorial/web/controllers/HomeController.java
@@ -3,7 +3,6 @@ package it.sevenbits.springboottutorial.web.controllers;
 import it.sevenbits.springboottutorial.web.domain.UserForm;
 import it.sevenbits.springboottutorial.web.service.*;
 
-import org.apache.catalina.startup.ClassLoaderFactory;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,11 +10,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.xml.ws.Service;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class HomeController {
