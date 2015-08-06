d80287ea8742892229cf73ad3ffcb4df3e5390f5
Uncrowned <ivashchenko.leo@gmail.com>
2015-08-06 15:06:40 +0700

    Revert "modified constraint for usernote, delete user -> delete his notes"
    This reverts commit a7d9bbb7b26e0e667391b4510cced4beb1af91d9.

--- src/main/resources/db/migration/V2015.08.06.14.37__Alter_usernotes_on_delete_.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE usernotes DROP CONSTRAINT usernotes_user_id_fkey;
-
-ALTER TABLE usernotes ADD CONSTRAINT usernotes_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
