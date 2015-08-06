f61f624ae42b642685a8b2144faa52780c9d2d4d
vik <xperia.v.j.p@gmail.com>
2015-08-06 13:14:30 +0600

    conducted lettle refactoring in css and js files

--- src/main/resources/public/js/myJS/addNote.js
+++ src/main/resources/public/js/myJS/addNote.js
@@ -7,53 +7,26 @@
 				$('#emptyList').remove();
 			};
 
-			var divCell = document.createElement("div");
-			divCell.setAttribute("class", "cell");
+			var cell = "<div id='new' class='cell' style='width: 0px; margin-top: 123px; height: 2px;'>"+
+				"<div class='content'></div>"+
+				"<div class='control'>"+
+					"<button class='delBtn' style='visibility: hidden;'></button>"+
+					"<button class='shaBtn' style='visibility: hidden;'></button>"+
+				"</div>"+
+			"</div>"
 			
-			divCell.setAttribute("id", "-1");
+			$(".noteDiv").prepend(cell);
 
-			var divContent = document.createElement("div");
-			divContent.setAttribute("class", "content");
+			cell = $('#new');
 
-			// var textarea = document.createElement("textarea");
-			// textarea.setAttribute("name", "text");
-			// textarea.setAttribute("maxlength", "20000");
-			// textarea.setAttribute('style', 'padding: 0px');
+			cell.animate({width: '350'}, 150, 'swing');
 
-			//divContent.appendChild(textarea);
-				
-			var btnDel = document.createElement("button");
-			btnDel.classList.add("delBtn");
-			btnDel.setAttribute('style', 'visibility: hidden;');
-
-			var shaBtn = document.createElement("button");			
-			shaBtn.classList.add("shaBtn");
-			shaBtn.setAttribute('style', 'visibility: hidden;');
-
-
-			var control = document.createElement("div");
-			control.classList.add("control");
-
-			control.appendChild(btnDel);
-			control.appendChild(shaBtn);
-
-			divCell.appendChild(divContent);
-			divCell.appendChild(control);
-			divCell.setAttribute('style', 'width: 0px; margin-top: 123px; height: 2px;');
-
-			$(".noteDiv").prepend(divCell);
-
-			divCell = $('#-1');
-
-			divCell.animate({width: '350'}, 150, 'swing');
-
-			divCell.animate({
+			cell.animate({
 					height: '200px',
 					marginTop: '0px'},
 				150, 'swing', function() {
-					$(divCell).children('.control').children('.delBtn').css('visibility', 'inherit');
-					$(divCell).children('.control').children('.shaBtn').css('visibility', 'inherit');
-					//textarea.removeAttribute('style', 'padding: 0px');
+					$(cell).children().find('.delBtn').css('visibility', 'inherit');
+					$(cell).children().find('.shaBtn').css('visibility', 'inherit');
 				}
 			)
 
@@ -64,7 +37,7 @@
 				};
 
 				App.Note.save(data, function(id) {
-					document.getElementById('-1').setAttribute('id', id);
+					$('#new').attr('id', id);
 				});				
 			})
 
