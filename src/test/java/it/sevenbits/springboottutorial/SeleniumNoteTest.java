af0ea86cd676b362d2efde6113e0db95ca0d3953
Julia <julia.novikova92@gmail.com>
2015-08-13 16:40:59 +0600

    improved create and delete test

--- src/test/java/it/sevenbits/springboottutorial/SeleniumNoteTest.java
+++ src/test/java/it/sevenbits/springboottutorial/SeleniumNoteTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -18,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 //import static org.hamcrest.Matchers.*;
+import java.awt.*;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -26,8 +29,113 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 
-/*@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@WebIntegrationTest*/
+@WebIntegrationTest
 public class SeleniumNoteTest {
+
+    private static WebDriver driver;
+
+    private static UserDetailsImpl user;
+
+    @Autowired
+    @Qualifier(value = "theUserPersistRepository")
+    public IUserRepository repository;
+
+
+    @BeforeClass
+    public static void initDriver() {
+        //driver = new ChromeDriver();
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        user = new UserDetailsImpl();
+        user.setEmail("ololo@ololo.com");
+        user.setUsername("Capitan");
+        //driver.manage().timeouts().setScriptTimeout(10, TimeUnit.SECONDS);
+    }
+
+    @AfterClass
+    public static void closeDriver() {
+        driver.close();
+    }
+
+    @Before
+    public void before() throws Exception {
+       try {
+            user.setPassword((new BCryptPasswordEncoder()).encode("Ololo73"));
+
+            repository.create(user);
+
+            user.setPassword("Ololo73");
+        } catch (Exception ex) {
+            fail(ex.getMessage());
+        }
+
+        driver.get("http://127.0.0.1:9000");
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        ExpectedCondition e = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return ((JavascriptExecutor)d).executeScript("return document.readyState").equals("complete");
+            }
+        };
+        wait.until(e);
+
+        WebElement email = driver.findElement(By.ByCssSelector.cssSelector("form[name=signinForm] input[name=username]"));
+        WebElement password = driver.findElement(By.ByCssSelector.cssSelector("form[name=signinForm] input[name=password]"));
+        WebElement submit = driver.findElement(By.ByCssSelector.cssSelector("form[name=signinForm] .loginSubmit"));
+
+        email.sendKeys(user.getEmail());
+        password.sendKeys(user.getPassword());
+        submit.submit();
+
+        assertTrue(driver.getCurrentUrl().equals("http://127.0.0.1:9000/telenote"));
+    }
+    @Test
+    public void createDeleteNoteTest() {
+
+        driver.findElement(By.className("addNote")).click();
+
+        assertFalse(driver.findElements(By.className("cell")).isEmpty());
+
+        WebElement el = driver.findElement(By.className("cell"));
+        driver.moveToElement(el);
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        ExpectedCondition e = new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                WebElement el = driver.findElement(By.className("cell"));
+                WebElement controlPanel = el.findElement(By.className("control"));
+                return !controlPanel.getCssValue("visibility").equals("hidden");
+            }
+        };
+        wait.until(e);
+
+        WebElement button = el.findElement(By.className("delBtn"));
+        driver.moveToElement(button);
+        button.click();
+
+        /*  driver.findElement(By.cssSelector("div.noteDiv.ui-sortable")).click();
+        driver.findElement(By.cssSelector("img")).click();
+        driver.findElement(By.cssSelector("button.delBtn")).click();*/
+
+
+        /*Actions builder = new Actions(driver);
+        builder.moveToElement(driver.findElement(By.className("cell ui-sortable-handle"))).perform();
+        By locator = By.className("delBtn");
+        driver.findElement(locator).click();
+        assertTrue(driver.findElements(By.className("cell")).isEmpty());*/
+
+
+
+    }
+    /*@Test
+    public void deleteNoteTest() {
+        driver.findElement(By.className("addNote")).click();
+
+        assertFalse(driver.findElements(By.className("cell")).isEmpty());
+
+    }*/
 }
