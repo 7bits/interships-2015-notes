84f0b72740fce8760beb63848ec08b0bd22c50b2
vik <xperia.v.j.p@gmail.com>
2015-08-14 11:20:35 +0600

    added errors for account page

--- src/main/resources/public/js/myJS/account.js
+++ src/main/resources/public/js/myJS/account.js
@@ -21,37 +21,37 @@
 
 		//подсветка неверных полей
 		$(function() {
-			if ($('#js-name') != null) {
-				$('.username').css('style', 'border: 1px solid #f74e19;')
-				$('#js-nameDiv').css('style', 'margin-left: 0;').animate({
+			if ($('#js-name')[0] != null) {
+				$('.username').attr('style', 'border: 1px solid #f74e19;')
+				$('#js-nameDiv').attr('style', 'margin-left: 115px;').animate({
 						marginLeft: '327px'
 					}, 200, 'swing', function() {
 						$('#js-nameDiv').removeAttr('style');
 				});
 			} else {
-				$('.username').css('style', 'border: 1px solid #ffffff;');
+				$('.username').attr('style', 'border: 1px solid #ffffff;');
 			};
 
-			if ($('#js-oldPass') != null) {
-				$('.currentPass').css('style', 'border: 1px solid #f74e19;');
-				$('#js-errorDiv').css('style', 'margin-left: 0;').animate({
+			if ($('#js-oldPass')[0] != null) {
+				$('.currentPass').attr('style', 'border: 1px solid #f74e19;');
+				$('#js-errorDiv').attr('style', 'margin-left: 248px;').animate({
 						marginLeft: '327px'
 					}, 200, 'swing', function() {
 						$('#js-errorDiv').removeAttr('style');
 				});
 			} else {
-				$('.currentPass').css('style', 'border: 1px solid #ffffff;');
+				$('.currentPass').attr('style', 'border: 1px solid #ffffff;');
 			};
 
-			if ($('#js-newPass') != null) {
-				$('.newPass').css('style', 'border: 1px solid #f74e19;');
-				$('#js-errorDiv').css('style', 'margin-left: 0;').animate({
+			if ($('#js-newPass')[0] != null) {
+				$('.newPass').attr('style', 'border: 1px solid #f74e19;');
+				$('#js-errorDiv').attr('style', 'margin-left: 248px;').animate({
 						marginLeft: '327px'
 					}, 200, 'swing', function() {
 						$('#js-errorDiv').removeAttr('style');
 				});
 			} else {
-				$('.newPass').css('style', 'border: 1px solid #ffffff;');
+				$('.newPass').attr('style', 'border: 1px solid #ffffff;');
 			};			
 		})
 	})
