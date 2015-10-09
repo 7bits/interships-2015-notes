aca711ef0c70af9cc7f8e34acba21fd8dfbe0cc6
vik <xperia.v.j.p@gmail.com>
2015-10-09 15:20:22 +0600

    fixed bug with space in user name

--- redeploy.sh
+++ redeploy.sh
@@ -1,3 +1,3 @@
 maven -Pnotest tomcat7:redeploy
-gulp css-staging
-scp -r src/main/resources/public notes@192.168.200.112:/home/notes/Notes/target/classes/
+gulp build-staging
+scp -r src/main/resources/public root@192.168.200.112:/home/notes/Notes/target/classes/
